04b72defbce8b72916ebccb52c110b4c915312e6
dont-wait <nguyentansang15042005@gmail.com>
2026-02-04 14:36:21 +0700

    add config flutter | config automatic delete old version in nixos | add config docker

---
+++ nixos/languages.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+
+{
+  # Tại đây khai báo các môi trường lập trình và LSP Server
+  environment.systemPackages = with pkgs; [
+    # --- C/C++ ---
+    gcc
+    clang
+    # clang-tools # bao gồm clangd (LSP)
+
+    # --- Node/Web ---
+    nodejs
+    nodePackages.typescript-language-server # LSP cho Typescript/Javascript
+    nodePackages.vscode-langservers-extracted # Chứa LSP cho HTML, CSS, JSON, ESLint
+
+    # --- Lua ---
+    lua-language-server
+
+    # --- Go (Golang) ---
+    go
+    gopls # LSP chính chủ cho Go
+
+    # --- Python ---
+    python3
+    pyright # LSP cho Python
+
+    # --- Nix ---
+    nil # LSP cho Nix
+
+    # --- Shell/Bash ---
+    shfmt # Formatter
+    shellcheck # Linter
+    bash-language-server # LSP cho Bash
+
+    # Flutter or others, lazyyyy
+    flutter
+    ninja
+    pkg-config
+    docker-compose
+    lazydocker
+  ];
+}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -116,3 +116,10 @@ alias "sudo rm"="rm -i"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 # [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers --color=always {}'"
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
