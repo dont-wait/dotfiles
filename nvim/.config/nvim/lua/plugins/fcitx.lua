ab62fd6ba298eb083ae1bd7f674974fcbee47a39
dont-wait <nguyentansang15042005@gmail.com>
2026-03-05 22:37:16 +0700

    fix: force en mode when quit buff scree

--- nvim/.config/nvim/lua/plugins/fcitx.lua
+++ nvim/.config/nvim/lua/plugins/fcitx.lua
@@ -1,6 +1,7 @@
 return {
-    {
-        "h-hg/fcitx.nvim",
-        event = "InsertEnter",
-    },
+    "h-hg/fcitx.nvim",
+    event = "InsertEnter",
+    config = function()
+        vim.g.fcitx5_remote = "fcitx5-remote"
+    end,
 }
