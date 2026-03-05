3de921f191f4348957ddeb95f8843d01cc79a6a2
dont-wait <nguyentansang15042005@gmail.com>
2026-03-05 14:31:13 +0700

    add copilot lua

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    keymap = {
+                        accept = "<M-u>",
+                        accept_word = "<M-w>",
+                        accept_line = "<M-l>",
+                        next = "<M-]>",
+                        prev = "<M-[>",
+                        dismiss = "<C-]>",
+                    },
+                },
+                panel = { enabled = false },
+            })
+        end,
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    },
+}
