e6102e906a241fc6056c6b5a00158859c9634b86
dont-wait <nguyentansang15042005@gmail.com>
2026-03-17 21:35:39 +0700

    fuk path in nixos

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-sleep 2
+#!/usr/bin/env bash
+until i3-msg > /dev/null 2>&1; do
+    sleep 0.5
+done
+sleep 0.5
 pkill -x polybar
-sleep 1
 polybar main 2>&1 | tee -a /tmp/polybar.log & disown
