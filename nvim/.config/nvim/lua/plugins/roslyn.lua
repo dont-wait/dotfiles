63f38f1f28916e55eaf89eaa5712e5f82493c2a4
dont-wait <nguyentansang15042005@gmail.com>
2026-04-09 13:58:05 +0700

    sync

--- nvim/.config/nvim/lua/plugins/roslyn.lua
+++ nvim/.config/nvim/lua/plugins/roslyn.lua
@@ -1,53 +1,57 @@
 -- lua/plugins/roslyn.lua
 return {
 	"seblyng/roslyn.nvim",
-	---@module 'roslyn.config'
-	---@type RoslynNvimConfig
 	ft = { "cs" },
 	opts = {
 		filewatching = "roslyn",
 		broad_search = true,
 		lock_target = false,
 		silent = false,
-
 		config = {
 			cmd = { "roslyn-ls" },
+			capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), {
+				textDocument = {
+					inlayHint = { dynamicRegistration = true },
+				},
+			}),
 			on_attach = function(client, bufnr)
-				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+				vim.defer_fn(function()
+					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+				end, 3000)
 			end,
-			settings = {
-				["csharp|inlay_hints"] = {
-					csharp_enable_inlay_hints_for_implicit_object_creation = true,
-					csharp_enable_inlay_hints_for_implicit_variable_types = true,
-					csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-					csharp_enable_inlay_hints_for_types = true,
-					dotnet_enable_inlay_hints_for_indexer_parameters = true,
-					dotnet_enable_inlay_hints_for_literal_parameters = true,
-					dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-					dotnet_enable_inlay_hints_for_other_parameters = true,
-					dotnet_enable_inlay_hints_for_parameters = true,
-					dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-					dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-				},
-				["csharp|code_lens"] = {
-					dotnet_enable_references_code_lens = true,
-					dotnet_enable_tests_code_lens = true,
-				},
-				["csharp|completion"] = {
-					dotnet_show_completion_items_from_unimported_namespaces = true,
-					dotnet_show_name_completion_suggestions = true,
-					dotnet_provide_regex_completions = true,
-				},
-				["csharp|background_analysis"] = {
-					dotnet_analyzer_diagnostics_scope = "fullSolution",
-					dotnet_compiler_diagnostics_scope = "fullSolution",
-				},
-				["csharp|formatting"] = {
-					dotnet_organize_imports_on_format = true,
-				},
-				["csharp|symbol_search"] = {
-					dotnet_search_reference_assemblies = true,
-				},
+		},
+		settings = {
+			["csharp|inlay_hints"] = {
+				csharp_enable_inlay_hints_for_implicit_object_creation = true,
+				csharp_enable_inlay_hints_for_implicit_variable_types = true,
+				csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+				csharp_enable_inlay_hints_for_types = true,
+				dotnet_enable_inlay_hints_for_indexer_parameters = true,
+				dotnet_enable_inlay_hints_for_literal_parameters = true,
+				dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+				dotnet_enable_inlay_hints_for_other_parameters = true,
+				dotnet_enable_inlay_hints_for_parameters = true,
+				dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+				dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+			},
+			["csharp|code_lens"] = {
+				dotnet_enable_references_code_lens = true,
+				dotnet_enable_tests_code_lens = true,
+			},
+			["csharp|completion"] = {
+				dotnet_show_completion_items_from_unimported_namespaces = true,
+				dotnet_show_name_completion_suggestions = true,
+				dotnet_provide_regex_completions = true,
+			},
+			["csharp|background_analysis"] = {
+				dotnet_analyzer_diagnostics_scope = "fullSolution",
+				dotnet_compiler_diagnostics_scope = "fullSolution",
+			},
+			["csharp|formatting"] = {
+				dotnet_organize_imports_on_format = true,
+			},
+			["csharp|symbol_search"] = {
+				dotnet_search_reference_assemblies = true,
 			},
 		},
 	},
